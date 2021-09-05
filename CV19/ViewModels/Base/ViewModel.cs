1529059bbe3d9678e31d9ba7e13403450511fa05
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-09-05 19:17:39 +0300

    1 - 32:10

---
+++ CV19/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿#nullable enable
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CV19.ViewModels.Base
+{
+  public abstract class ViewModel: INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    /// <summary>
+    /// Кольцевое обновление свойства
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="field"></param>
+    /// <param name="value"></param>
+    /// <param name="propertyName"></param>
+    /// <returns>bool</returns>
+    protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+      if (Equals(field, value)) return false; // выходим, если значение не изменилось
+
+      field = value;
+      OnPropertyChanged(propertyName);
+      return true;
+    }
+  }
+}
