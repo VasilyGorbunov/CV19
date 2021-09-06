4446cc0c06204577149ea1b840b0a1fe6f802763
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-09-06 18:01:50 +0300

    1 - 1:59:00

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CV19Console
 {
   class Program
   {
-    static void Main(string[] args)
+    private const string data_url =
+      @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    private static async Task<Stream> GetDataStream()
     {
-      Console.WriteLine("Hello World!");
+      var client = new HttpClient();
+      var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+      return await response.Content.ReadAsStreamAsync();
+    }
+
+    private static IEnumerable<string> GetDataLines()
+    {
+      using var data_stream =  GetDataStream().Result;
+      using var data_reader = new StreamReader(data_stream);
+
+      while (!data_reader.EndOfStream)
+      {
+        var line = data_reader.ReadLine();
+        if(string.IsNullOrWhiteSpace(line)) continue;
+        yield return line;
+      }
+    }
+
+    static void  Main(string[] args)
+    {
+      foreach (var data_line in GetDataLines())
+      {
+        Console.WriteLine(data_line);
+      }
+      
+      Console.ReadLine();
     }
   }
 }
