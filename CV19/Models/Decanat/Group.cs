439528b3d71bc034e56fca97c04b0176ef093198
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-09-07 17:49:13 +0300

    2 - 0:22:25

---
+++ CV19/Models/Decanat/Group.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CV19.Models.Decanat
+{
+  public class Group
+  {
+    public string Name { get; set; }
+    public ICollection<Student> Students { get; set; }
+  }
+}
---
+++ CV19/Models/Decanat/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CV19.Models.Decanat
+{
+  public class Student
+  {
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Patronymic { get; set; }
+    public DateTime Birthday { get; set; }
+    public double Rating { get; set; }
+  }
+}
