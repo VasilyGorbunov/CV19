ffd04ed6f951adadbcc2e4cd8a1bac439f8df857
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-09-07 18:10:41 +0300

    2 - 28:55

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using CV19.Infrastructure.Commands;
 using CV19.Models;
+using CV19.Models.Decanat;
 using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
@@ -11,6 +14,8 @@ namespace CV19.ViewModels
   public class MainWindowViewModel: ViewModel
   {
     #region Свойства
+
+    public ObservableCollection<Group> Groups { get; }
 
     #region Номер выбранной вкладки
     private int _selectedPageIndex = 0;
@@ -105,6 +110,26 @@ namespace CV19.ViewModels
 
     public MainWindowViewModel()
     {
+      #region Студенты
+
+      var student_index = 1;
+      var students = Enumerable.Range(1, 25).Select(i => new Student
+      {
+        Name = $"Name {student_index}",
+        Surname = $"Surname {student_index}",
+        Patronymic = $"Patronymic {student_index++}",
+        Birthday = DateTime.Now,
+        Rating = 0
+      });
+
+      var groups = Enumerable.Range(1, 50).Select(i => new Group
+      {
+        Name = $"Группа №{i}",
+        Students = new ObservableCollection<Student>(students)
+      });
+      Groups = new ObservableCollection<Group>(groups);
+      #endregion
+
       #region Тестовый набор данных для визуализации графика
       var data_points = new List<DataPoint>((int) (360 / 0.1));
       for (var x = 0d; x <= 360; x += 0.1)
