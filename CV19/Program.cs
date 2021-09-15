90e614c0400ac96fff2d219203e950ac42058ac2
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-09-15 18:15:13 +0300

    5 - 56:33

---
+++ CV19/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CV19
+{
+  public static class Program
+  {
+    [STAThread]
+    public static void Main(string[] args)
+    {
+      var app = new App();
+      app.InitializeComponent();
+      app.Run();
+    }
+  }
+}
