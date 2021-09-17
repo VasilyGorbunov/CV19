78baf091b862a54101f3bc331a50262da7744052
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-09-17 13:34:48 +0300

    5 - 1:14:00

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+﻿using System;
 using System.Windows;
 using CV19.Services;
 using CV19.ViewModels;
@@ -7,21 +7,38 @@ using Microsoft.Extensions.Hosting;
 
 namespace CV19
 {
-  public partial class App : Application
+  public partial class App
   {
+    private static IHost _host;
+
     public static bool IsDedignMode { get; private set; } = true;
 
-    protected override void OnStartup(StartupEventArgs e)
+    protected override async void OnStartup(StartupEventArgs e)
     {
       IsDedignMode = false;
+      var host = Host;
       base.OnStartup(e);
+
+      await host.StartAsync().ConfigureAwait(false);
     }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+      base.OnExit(e);
+      var host = Host;
+      await host.StopAsync().ConfigureAwait(false);
+      host.Dispose();
+      _host = null;
+    }
+
+    public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
 
     public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
     {
       services.AddSingleton<DataService>();
 
       services.AddSingleton<CountriesStatisticViewModel>();
+
     }
   }
 }
