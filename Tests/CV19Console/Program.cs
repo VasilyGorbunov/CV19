482abd0a92f8e86ca6a6440719b1e55a2d234aec
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-09-21 17:26:42 +0300

    6 - 0:17:00

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -4,71 +4,35 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CV19Console
 {
   class Program
   {
-    private const string data_url =
-      @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-
-    private static async Task<Stream> GetDataStream()
-    {
-      var client = new HttpClient();
-      var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
-      return await response.Content.ReadAsStreamAsync();
-    }
-
-    private static IEnumerable<string> GetDataLines()
-    {
-      using var data_stream =  GetDataStream().Result;
-      using var data_reader = new StreamReader(data_stream);
-
-      while (!data_reader.EndOfStream)
-      {
-        var line = data_reader.ReadLine();
-        if(string.IsNullOrWhiteSpace(line)) continue;
-        yield return line
-          .Replace("Korea,", "Korea -")
-          .Replace("Bonaire,", "Bonaire -");
-      }
-    }
-
-    private static DateTime[] GetDates() => GetDataLines()
-      .First()
-      .Split(',')
-      .Skip(4)
-      .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
-      .ToArray();
-
-    private static IEnumerable<(string country, string province, int[] counts)> GetData()
-    {
-      var lines = GetDataLines()
-        .Skip(1)
-        .Select(line => line.Split(','));
-
-      foreach (var row in lines)
-      {
-        var province = row[0].Trim();
-        var country_name = row[1].Trim(' ', '"');
-        var counts = row.Skip(4).Select(int.Parse).ToArray();
-
-        yield return (country_name, province, counts);
-      }
-
-    }
-    
-
     static void  Main(string[] args)
     {
-      var russia_data = GetData()
-        .First(v => v.country.Equals("Russia", StringComparison.InvariantCulture));
+      Thread.CurrentThread.Name = "Main Thread";
 
-      Console.WriteLine(string.Join("\r\n", GetDates().Zip(
-          russia_data.counts, (date, count) => $"{date:dd.MM.yyyy} - {count}")));
-      
+      var thread = new Thread(ThreadMethod);
+      thread.Name = "Other thread";
+      thread.Start();
+
+      CheckThread();
+
       Console.ReadLine();
+    }
+
+    private static void ThreadMethod()
+    {
+      CheckThread();
+    }
+
+    public static void CheckThread()
+    {
+      var thread = Thread.CurrentThread;
+      Console.WriteLine($"{thread.ManagedThreadId}:{thread.Name}");
     }
   }
 }
