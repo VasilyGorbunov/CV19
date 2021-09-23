01e66e344aea7953b75b887052ed9ad7d5a4e2f2
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-09-23 19:29:55 +0300

    46:15

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace CV19Console
@@ -9,24 +10,44 @@ namespace CV19Console
     {
       Thread.CurrentThread.Name = "Main Thread";
 
-      var thread = new Thread(ThreadMethod);
-      thread.Name = "Other thread";
-      thread.IsBackground = true;
-      thread.Start(42);
+      //var thread = new Thread(ThreadMethod);
+      //thread.Name = "Other thread";
+      //thread.IsBackground = true;
+      //thread.Start(42);
 
-      var count = 5;
-      var msg = "Hello Thread";
-      var tm = 150;
+      //var count = 5;
+      //var msg = "Hello Thread";
+      //var tm = 150;
 
-      new Thread(() => PrintMethod(msg, count, tm)) {IsBackground = true}.Start();
+      //new Thread(() => PrintMethod(msg, count, tm)) {IsBackground = true}.Start();
 
-      CheckThread();
+      //CheckThread();
 
-      for (var i = 0; i < 10; i++)
+      //for (var i = 0; i < 10; i++)
+      //{
+      //  Thread.Sleep(100);
+      //  Console.WriteLine(i);
+      //}
+
+      var values =   new List<int>();
+      var threads = new Thread[10];
+      for (int i = 0; i < threads.Length; i++)
       {
-        Thread.Sleep(100);
-        Console.WriteLine(i);
+        threads[i] = new Thread(() =>
+        {
+          for(int j = 0; j < 10; j++)
+            values.Add(Thread.CurrentThread.ManagedThreadId);
+        });
       }
+
+      foreach (var thread in threads)
+      {
+        thread.Start();
+      }
+
+
+      Console.ReadLine();
+      Console.WriteLine(string.Join(",", values));
       Console.ReadLine();
     }
 
