c06b858a17bb0c67efb40373cf67acab5fe3a18b
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-10-11 17:40:15 +0300

    1:58:00

---
+++ CV19/Infrastructure/Behaviors/CloseWindow.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.Xaml.Behaviors;
+
+namespace CV19.Infrastructure.Behaviors
+{
+  public class CloseWindow : Behavior<Button>
+  {
+    protected override void OnAttached()
+    {
+      AssociatedObject.Click += OnButtonClick;
+    }
+
+    protected override void OnDetaching()
+    {
+      AssociatedObject.Click -= OnButtonClick;
+    }
+
+    private void OnButtonClick(object sender, RoutedEventArgs e)
+    {
+      (AssociatedObject.FindVisualRoot() as Window)?.Close();
+    }
+
+    
+  }
+}
