285476dd7948d7a38b899e88183528f60b924e16
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-10-15 22:16:30 +0300

    7-done

--- CV19/Infrastructure/Extensions/WindowExtension.cs
+++ CV19/Infrastructure/Extensions/WindowExtension.cs
@@ -11,7 +11,13 @@ namespace System.Windows
     [DllImport(user32, CharSet = CharSet.Auto)]
     private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 
-    public static IntPtr SendMessage(this Window window, WM Msg, SC wParam, IntPtr lParam = default) => 
+    [DllImport(user32, CharSet = CharSet.Auto)]
+    private static extern IntPtr SendMessage(IntPtr hWnd, WM Msg, IntPtr wParam, IntPtr lParam);
+
+    public static IntPtr SendMessage(this Window window, WM Msg, IntPtr wParam, IntPtr lParam) => 
+      SendMessage(window.GetWindowHandle(), Msg, wParam, lParam);
+
+    public static IntPtr SendMessage(this Window window, WM Msg, SC wParam, IntPtr lParam = default) =>
       SendMessage(window.GetWindowHandle(), (uint)Msg, (IntPtr)wParam, lParam == default ? (IntPtr)' ' : lParam);
 
     public static IntPtr GetWindowHandle(this Window window)
