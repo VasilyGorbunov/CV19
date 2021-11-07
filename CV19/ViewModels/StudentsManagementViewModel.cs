52956c3a07d85dbb702b7b4065cd779b8a150c34
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-11-07 16:26:47 +0300

    0:32:30

---
+++ CV19/ViewModels/StudentsManagementViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CV19.ViewModels.Base;
+
+namespace CV19.ViewModels
+{
+  public class StudentsManagementViewModel : ViewModel
+  {
+    #region Title: string - Заголовок окна
+    /// <summary>
+    /// Заголовок окна
+    /// </summary>
+    private string _title = "Управление студентами";
+
+    /// <summary>
+    /// Заголовок окна
+    /// </summary>
+    public string Title
+    {
+      get => _title;
+      set => Set(ref _title, value);
+    } 
+    #endregion
+  }
+}
--- CV19/ViewModels/ViewModelLocator.cs
+++ CV19/ViewModels/ViewModelLocator.cs
@@ -4,6 +4,10 @@ namespace CV19.ViewModels
 {
   public class ViewModelLocator
   {
-    public MainWindowViewModel MainWindowModel => App.Host.Services.GetRequiredService<MainWindowViewModel>();
+    public MainWindowViewModel MainWindowModel => 
+      App.Host.Services.GetRequiredService<MainWindowViewModel>();
+
+    public StudentsManagementViewModel StudentManagement =>
+      App.Host.Services.GetRequiredService<StudentsManagementViewModel>();
   }
 }
