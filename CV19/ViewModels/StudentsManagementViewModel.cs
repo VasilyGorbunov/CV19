34285c7e0e8120487a044921c849179550ef0e31
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-11-07 17:31:01 +0300

    1:03:40

--- CV19/ViewModels/StudentsManagementViewModel.cs
+++ CV19/ViewModels/StudentsManagementViewModel.cs
@@ -1,9 +1,14 @@
-﻿using CV19.ViewModels.Base;
+﻿using System.Collections.Generic;
+using CV19.Models.Decanat;
+using CV19.Services.Students;
+using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
 {
   public class StudentsManagementViewModel : ViewModel
   {
+    private readonly StudentsManager _studentsManager;
+
     #region Title: string - Заголовок окна
     /// <summary>
     /// Заголовок окна
@@ -17,7 +22,28 @@ namespace CV19.ViewModels
     {
       get => _title;
       set => Set(ref _title, value);
+    }
+    #endregion
+
+    #region SelectedGroup: Group - Выбранная группа
+    /// <summary>
+    /// Выбранная группа
+    /// </summary>
+    private Group _selectedGroup;
+
+    /// <summary>
+    /// Выбранная группа
+    /// </summary>
+    public Group SelectedGroup
+    {
+      get => _selectedGroup;
+      set => Set(ref _selectedGroup, value);
     } 
     #endregion
+
+    public IEnumerable<Student> Students => _studentsManager.Students;
+    public IEnumerable<Group> Groups => _studentsManager.Groups;
+
+    public StudentsManagementViewModel(StudentsManager studentsManager) => _studentsManager = studentsManager;
   }
 }
