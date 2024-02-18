6a20fbf005e0f73be40756666796d2c081dc323b
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-02-18 19:00:58 +0300

    01 - 1:24:00

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,17 @@
-﻿using CV19.ViewModels.Base;
+﻿using CV19.Infrastructure.Commands.Base;
+using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
-
 
         #region Properties
 
@@ -22,10 +24,36 @@ namespace CV19.ViewModels
         /// Заголовок окна
         /// </summary>
         public string Title { get => _Title; set => Set(ref _Title, value); }
-        #endregion 
+        #endregion
+
+        #region Status : string - Статус программы
+        /// <summary>Статус программы</summary>
+        private string _Status = "Готов!";
+        /// <summary>Статус программы</summary>
+        public string Status { get => _Status; set => Set(ref _Status, value); }
+        #endregion
 
         #endregion
 
+        #region Commands
 
+        #region Close Application Command
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p) 
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Commands
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            #endregion
+        }
     }
 }
