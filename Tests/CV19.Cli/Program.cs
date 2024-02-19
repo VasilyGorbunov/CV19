c4ebaf63503f9c6b155780e4fe25527defc94442
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-02-19 12:14:27 +0300

    01 - 1:50:30

--- Tests/CV19.Cli/Program.cs
+++ Tests/CV19.Cli/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+HttpClient client = new HttpClient();
+
+HttpResponseMessage response = client.GetAsync(data_url).GetAwaiter().GetResult();
+string csv_str = response.Content.ReadAsStringAsync().Result;
+
+Console.ReadLine();
