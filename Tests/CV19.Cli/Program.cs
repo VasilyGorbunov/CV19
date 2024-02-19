ecdebb0c7a00392a5ceeb4aac85617c76a407663
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-02-19 13:39:22 +0300

    01 - 1:59:20

--- Tests/CV19.Cli/Program.cs
+++ Tests/CV19.Cli/Program.cs
@@ -1,8 +1,31 @@
-﻿const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+﻿using System.Linq;
 
-HttpClient client = new HttpClient();
+const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-HttpResponseMessage response = client.GetAsync(data_url).GetAwaiter().GetResult();
-string csv_str = response.Content.ReadAsStringAsync().Result;
+static async Task<Stream> GetDataStream()
+{
+    HttpClient client = new HttpClient();
+    HttpResponseMessage response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+    return await response.Content.ReadAsStreamAsync();
+}
+
+static IEnumerable<string> GetDataLines()
+{
+    using Stream? data_stream = GetDataStream().Result;
+    using StreamReader? data_reader = new StreamReader(data_stream);
+
+    while(!data_reader.EndOfStream)
+    {
+        string? line = data_reader.ReadLine();
+        if (string.IsNullOrWhiteSpace(line)) continue;
+        yield return line;
+    }
+}
+
+
+foreach (string line in GetDataLines())
+{
+    Console.WriteLine(line);
+}
 
 Console.ReadLine();
